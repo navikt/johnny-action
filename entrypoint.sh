0cfe18175bbda37d9f4e0788825e68250ef54cdc
Johnny Horvi <johnny@horvi.no>
2023-12-13 10:07:32 +0100

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,14 @@
 
 echo "Hello $1"
 
+echo "url: $ACTION_ID_TOKEN_REQUEST_URL"
+echo "token: $ACTION_ID_TOKEN_REQUEST_TOKEN"
+
+payload=$(curl -H "Authorization: bearer $ACTIONS_ID_TOKEN_REQUEST_TOKEN" "$ACTIONS_ID_TOKEN_REQUEST_URL&audience=hookd")
+
+echo "payload=$payload"
+
+echo "payload=$payload" >> $GITHUB_OUTPUT
+
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
